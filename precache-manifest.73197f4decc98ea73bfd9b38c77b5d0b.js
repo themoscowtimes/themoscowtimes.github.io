64cb81c8d52b4eefdf367f1a7016f307f966c761
misha <interlekt@gmail.com>
2019-03-13 12:12:53 -0400

    build

--- precache-manifest.73197f4decc98ea73bfd9b38c77b5d0b.js
+++ precache-manifest.73197f4decc98ea73bfd9b38c77b5d0b.js
@@ -4,15 +4,15 @@ self.__precacheManifest = [
     "url": "/static/js/runtime~main.a8a9905a.js"
   },
   {
-    "revision": "d98224f4ee159d804f9b",
-    "url": "/static/js/main.e5b4e75f.chunk.js"
+    "revision": "6d255e6ed026af2bb550",
+    "url": "/static/js/main.78f507f6.chunk.js"
   },
   {
     "revision": "ee1d970a8d21774074ec",
     "url": "/static/js/2.446d0871.chunk.js"
   },
   {
-    "revision": "3e0cde944a32e71375dc9fefe1367be3",
+    "revision": "4ca7848c436aa4fdc2cf90aa75fcbe6a",
     "url": "/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9a7a57fd88d03ac7fbd5d5fe2edb95e9.js"
+  "/precache-manifest.73197f4decc98ea73bfd9b38c77b5d0b.js"
 );
 
 workbox.clientsClaim();
