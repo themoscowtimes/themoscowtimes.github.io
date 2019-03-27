1067c9969f2d2693f9d85dc667b238ecb3938e8f
mtdevbeta <mtdevbeta@github>
2019-03-27 14:02:32 +0300

    build

--- precache-manifest.023a465a9266cc14a5f6c45296c5bfda.js
+++ precache-manifest.023a465a9266cc14a5f6c45296c5bfda.js
@@ -8,15 +8,15 @@ self.__precacheManifest = [
     "url": "/static/js/runtime~main.a8a9905a.js"
   },
   {
-    "revision": "19b2d4901a474797e787",
-    "url": "/static/js/main.0da0c562.chunk.js"
+    "revision": "a2f377c6a90c38bb2160",
+    "url": "/static/js/main.79bfeb45.chunk.js"
   },
   {
     "revision": "bece5b8d81321e9842b3",
     "url": "/static/js/2.222520a7.chunk.js"
   },
   {
-    "revision": "716df7e755cc2990b355efefd5859c04",
+    "revision": "ed49199834e2f1a25b38ce7ac3b7faba",
     "url": "/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.95b61b01a63ebd59291bb9784208d83a.js"
+  "/precache-manifest.023a465a9266cc14a5f6c45296c5bfda.js"
 );
 
 workbox.clientsClaim();
