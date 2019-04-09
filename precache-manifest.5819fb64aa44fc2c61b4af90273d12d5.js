5a26f4fb66bae65642dbe89e43a62437b81393f0
misha <interlekt@gmail.com>
2019-04-09 10:57:21 -0400

    build

---
+++ precache-manifest.5819fb64aa44fc2c61b4af90273d12d5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8a7394ad20bb48f7e248a9b503e34743",
+    "url": "/static/media/placeholder.8a7394ad.jpg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "a93db178c64f185d4173",
+    "url": "/static/js/main.ff8bc958.chunk.js"
+  },
+  {
+    "revision": "afe21dadaabe3b3bb5f2",
+    "url": "/static/js/2.4ebca6b3.chunk.js"
+  },
+  {
+    "revision": "bdb10405d8d9a3f7f790bca9c7d3d5d8",
+    "url": "/index.html"
+  }
+];
