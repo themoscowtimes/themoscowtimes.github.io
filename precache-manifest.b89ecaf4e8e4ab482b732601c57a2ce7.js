bc8594cd15871bef1e26eceadea34f3d1f1f744c
mtdevbeta <mtdevbeta@github>
2019-04-30 14:49:21 +0300

    build

---
+++ precache-manifest.b89ecaf4e8e4ab482b732601c57a2ce7.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9597d181ba9eb1ddce2ab172f30fc5d9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3ac24a63b339bbdc8e30",
+    "url": "/static/js/2.f9a0e119.chunk.js"
+  },
+  {
+    "revision": "73d2be8724032a61b110",
+    "url": "/static/js/main.cf817ccc.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "c76f8780b8250cf7f301782da8ecea9e",
+    "url": "/static/media/native_ads.c76f8780.png"
+  },
+  {
+    "revision": "c6df8eb713751420a56845c5cd93214f",
+    "url": "/static/media/native_ads_0.c6df8eb7.png"
+  },
+  {
+    "revision": "b0a6794604e30d3064144278105bb4cf",
+    "url": "/static/media/native_ads_1.b0a67946.png"
+  },
+  {
+    "revision": "bfd3e341dfee69428ce10c39f50b2a27",
+    "url": "/static/media/native_ads_2.bfd3e341.png"
+  },
+  {
+    "revision": "eb5c2407c197ec4b76cdee8b2430b9a9",
+    "url": "/static/media/native_ads_3.eb5c2407.png"
+  },
+  {
+    "revision": "8a7394ad20bb48f7e248a9b503e34743",
+    "url": "/static/media/placeholder.8a7394ad.jpg"
+  }
+]);
