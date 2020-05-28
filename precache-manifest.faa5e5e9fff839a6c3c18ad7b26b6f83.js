3956b9d57f7044db8020181090ce958f1d164210
delibash <interlekt@gmail.com>
2020-05-28 11:16:39 -0400

    build

---
+++ precache-manifest.faa5e5e9fff839a6c3c18ad7b26b6f83.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0cdcc78f260e9b5bb079ff2282e89293",
+    "url": "/index.html"
+  },
+  {
+    "revision": "193abff8625f40d6c262",
+    "url": "/static/js/2.b6f84239.chunk.js"
+  },
+  {
+    "revision": "335932bcfe373407d7c8acc8ad15eddd",
+    "url": "/static/js/2.b6f84239.chunk.js.LICENSE"
+  },
+  {
+    "revision": "df69403f042d64aac336",
+    "url": "/static/js/main.837b8d73.chunk.js"
+  },
+  {
+    "revision": "4318a88bbaf79c7de3c3",
+    "url": "/static/js/runtime~main.80f2aab6.js"
+  },
+  {
+    "revision": "8a7394ad20bb48f7e248a9b503e34743",
+    "url": "/static/media/placeholder.8a7394ad.jpg"
+  }
+]);
